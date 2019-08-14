5ab4287886f1686633592cb018c20043c209d4a6
Nick Riabovol <nick.riabovol@gmail.com>
2019-08-14 23:56:31 +0300

    I have solved the task RemoveElement27

---
+++ ua/nick/leetcode/easy/RemoveElement27.java
@@ -0,0 +1,95 @@
+package ua.nick.leetcode.easy;
+
+/*
+    * 27. Remove Element
+    Easy
+
+    Given an array nums and a value val, remove all instances of that value in-place and return
+    the new length.
+
+    Do not allocate extra space for another array, you must do this by modifying the input array
+     in-place with O(1) extra memory.
+
+    The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+
+    Example 1:
+
+    Given nums = [3,2,2,3], val = 3,
+
+    Your function should return length = 2, with the first two elements of nums being 2.
+
+    It doesn't matter what you leave beyond the returned length.
+
+    Example 2:
+
+    Given nums = [0,1,2,2,3,0,4,2], val = 2,
+
+    Your function should return length = 5, with the first five elements of nums containing
+    0, 1, 3, 0, and 4.
+
+    Note that the order of those five elements can be arbitrary.
+
+    It doesn't matter what values are set beyond the returned length.
+* */
+
+import java.util.Arrays;
+
+public class RemoveElement27 {
+
+    public static void main(String[] args) {
+        int[] nums = {4, 4};
+
+        System.out.println(removeElement(nums, 4));
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static int removeElement(int[] nums, int val) {
+        /*int newSize = nums.length, nextElem = 0, pointer;
+        for (int i = 0; i < nums.length; ++i) {
+            if (nums[i] == val) {
+                pointer = ++nextElem + i;
+                while (pointer < nums.length) {
+                    if (nums[pointer] != val) {
+                        nums[i] = nums[pointer];
+                        break;
+                    }
+                    ++pointer;
+                    ++nextElem;
+                }
+                if (pointer == nums.length) {
+                    break;
+                }
+            } else {
+                if (i + nextElem < nums.length) {
+                    while (i + nextElem < nums.length && nums[i + nextElem] == val) {
+                        ++nextElem;
+                    }
+
+                    if (nextElem + i + 1 >= nums.length) {
+                        break;
+                    }
+                    nums[i] = nums[i + nextElem];
+                } else {
+                    break;
+                }
+            }
+        }*/
+        int lastStart = nums.length - 1;
+
+        for (int i = 0; i < nums.length; ++i) {
+            if (nums[i] == val) {
+                while (lastStart >= 0 && nums[lastStart] == val) {
+                    --lastStart;
+                }
+                if (lastStart > i) {
+                    nums[i] = nums[lastStart];
+                    --lastStart;
+                } else {
+                    break;
+                }
+            }
+        }
+
+        return ++lastStart;
+    }
+}
