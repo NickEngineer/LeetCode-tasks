d8909991aba199c719bca1959e2b59d69e11fbd0
Nick Riabovol <nick.riabovol@gmail.com>
2019-08-26 23:19:14 +0300

    I have solved the task MergeSortedArray88

---
+++ ua/nick/leetcode/easy/MergeSortedArray88.java
@@ -0,0 +1,66 @@
+package ua.nick.leetcode.easy;
+
+/*
+    *88. Merge Sorted Array
+    Easy
+
+    Given two sorted integer arrays nums1 and nums2, merge nums2
+    into nums1 as one sorted array.
+
+    Note:
+
+    The number of elements initialized in nums1 and nums2
+    are m and n respectively.
+    You may assume that nums1 has enough space (size that
+    is greater or equal to m + n) to hold additional elements from nums2.
+    Example:
+
+    Input:
+    nums1 = [1,2,3,0,0,0], m = 3
+    nums2 = [2,5,6],       n = 3
+
+    Output: [1,2,2,3,5,6]
+* */
+
+import java.util.Arrays;
+
+public class MergeSortedArray88 {
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int[] nums2 = {2, 5, 6};
+
+        System.out.println(Arrays.toString(nums1));
+        System.out.println(Arrays.toString(nums2));
+
+        merge(nums1, 3, nums2, 3);
+
+        System.out.println(Arrays.toString(nums1));
+
+    }
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int[] temp = new int[m];
+        int pointer1 = 0, pointer2 = 0;
+
+        for (int i = 0; i < m; ++i) {
+            temp[i] = nums1[i];
+        }
+
+        for (int i = 0; i < m + n; ++i) {
+            if (pointer1 < m && pointer2 < n) {
+                if (temp[pointer1] <= nums2[pointer2]) {
+                    nums1[i] = temp[pointer1++];
+                } else {
+                    nums1[i] = nums2[pointer2++];
+                }
+            } else {
+                if (pointer1 >= m) {
+                    nums1[i] = nums2[pointer2++];
+                } else {
+                    nums1[i] = temp[pointer1++];
+                }
+            }
+        }
+    }
+}
