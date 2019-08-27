66cecf0cd325abbf029bf8446e23c887a1df57e5
Nick Riabovol <nick.riabovol@gmail.com>
2019-08-27 23:46:58 +0300

    I have solved the task LinkedListCycle141

---
+++ ua/nick/leetcode/easy/LinkedListCycle141.java
@@ -0,0 +1,80 @@
+package ua.nick.leetcode.easy;
+
+/*
+    *141. Linked List Cycle
+    Easy
+
+    Given a linked list, determine if it has a cycle in it.
+
+    To represent a cycle in the given linked list, we use an integer
+    pos which represents the position (0-indexed) in the linked list
+    where tail connects to. If pos is -1, then there is no cycle in
+    the linked list.
+
+
+    Example 1:
+
+    Input: head = [3,2,0,-4], pos = 1
+    Output: true
+    Explanation: There is a cycle in the linked list, where tail
+    connects to the second node.
+
+
+    Example 2:
+
+    Input: head = [1,2], pos = 0
+    Output: true
+    Explanation: There is a cycle in the linked list, where tail
+    connects to the first node.
+
+
+    Example 3:
+
+    Input: head = [1], pos = -1
+    Output: false
+    Explanation: There is no cycle in the linked list.
+
+* */
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LinkedListCycle141 {
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public static boolean hasCycle(ListNode head) {
+        boolean isCycle = false;
+        ListNode pointer = head, temp;
+        Set<Integer> values = new HashSet<>();
+        Set<ListNode> nodes = new HashSet<>();
+
+        if (pointer != null) {
+            while (pointer.next != null) {
+
+                if (!values.add(pointer.val)) {
+                    if (!nodes.add(pointer)) {
+                        isCycle = true;
+                        break;
+                    }
+                }
+
+                pointer = pointer.next;
+            }
+        }
+
+        return isCycle;
+    }
+}
